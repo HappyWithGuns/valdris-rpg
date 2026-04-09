2808db287255f341a22cc5808a99562484dac1e9
HappyWithGuns <joshuahorner125@gmail.com>
2026-04-08 21:40:57 -0700

    V0.0.4
    1. added basic stats for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,17 @@
 class_name Player
 extends CharacterBody2D
 
+@export var health : int
+
+@export var mana : int
+
 ## Holds a reference to the AnimationTree
 @export var anim_tree : AnimationTree
 
 ## Holds a reference to the StateMachine node
 @export var StateMachine : NodeStateController
+
+@export var movable : Movable
 
 var playback : AnimationNodeStateMachinePlayback
 
@@ -14,3 +20,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+
+func apply_force(force : float, acceleratio_time : float, length_of_tween : float) -> void:
+	var impluse := Vector2(force * acceleratio_time, force * acceleratio_time) * movable.get_input_vector()
+	create_tween().set_ease(Tween.EASE_IN).tween_property(self, "velocity", impluse, length_of_tween)
